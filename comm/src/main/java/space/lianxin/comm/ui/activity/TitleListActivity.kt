8fcb661d4404b8a997da311d5e4082f3c53e8383
lianxin <qingyizhuyin@qq.com>
2021-02-27 16:45:32 +0800

    新增标题列表结构的基类Activity

--- comm/src/main/java/space/lianxin/comm/ui/activity/TitleListActivity.kt
+++ comm/src/main/java/space/lianxin/comm/ui/activity/TitleListActivity.kt
@@ -1,10 +1,16 @@
 package space.lianxin.comm.ui.activity
 
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.CallSuper
+import com.airbnb.epoxy.EpoxyVisibilityTracker
+import space.lianxin.base.extention.click
 import space.lianxin.comm.databinding.ActivityTitleListBinding
 
 /**
  * ===========================================
+ * 标题列表结构的Activity的基类。
+ *
  * @author: lianxin
  * @date: 2021/2/25 14:00
  * ===========================================
@@ -13,14 +19,51 @@ abstract class TitleListActivity : ComMvRxAvtivity<ActivityTitleListBinding>() {
 
     override fun inflateBinding() = ActivityTitleListBinding.inflate(layoutInflater)
 
-
     @CallSuper
     override fun initView() {
-
+        initHeader()
+        initTitleBar(
+            binding.titleBar.titleTv,
+            binding.titleBar.rightTv,
+            binding.titleBar.rightIv,
+            binding.titleBar.backIv
+        )
         binding.recyclerView.setController(epoxyController)
+        if (needAttchToItemScroll()) {
+            attachToItemScroll()
+        }
     }
 
     override fun initData() {}
 
+    /** 初始化titleBar */
+    protected open fun initTitleBar(
+        title: TextView,
+        rightTv: TextView,
+        rightIv: ImageView,
+        backIv: ImageView
+    ) {
+    }
+
+    /** 是否需要关联到item的滑动(可以监听item滑动的距离和百分比等。) */
+    protected open fun needAttchToItemScroll(): Boolean = false
+
+    /** 设置头部返回键按钮点击事件 */
+    fun setHeaderBackClick(event: () -> Unit) {
+        binding.titleBar.backIv.click(event)
+    }
+
+    /** 初始化头部信息 */
+    private fun initHeader() {
+        setHeaderBackClick {
+            finish()
+        }
+        binding.refreshLayout.setEnableRefresh(true)
+        binding.refreshLayout.setEnableLoadMore(false)
+    }
+
+    private fun attachToItemScroll() {
+        EpoxyVisibilityTracker().attach(binding.recyclerView)
+    }
 
 }
